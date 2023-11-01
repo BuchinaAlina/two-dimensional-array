d10849bf3994132b710b5af4791de396d2545c67
Alina <buchina-alina@mail.ru>
2023-11-01 18:18:54 +1000

    pl

---
+++ Задача2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string originalString = "HeLLo WoRLd"; // Ваша исходная строка
+
+        // Заменяем все заглавные буквы на строчные
+        string modifiedString = originalString.ToLower();
+
+        Console.WriteLine("Исходная строка: " + originalString);
+        Console.WriteLine("Измененная строка: " + modifiedString);
+    }
+}
